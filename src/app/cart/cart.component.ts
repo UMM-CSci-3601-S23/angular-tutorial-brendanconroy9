300c103f9b5871573812d7ae9cbba63fbcc3cbc9
Brendan Conroy <conro188@morris.umn.edu>
2023-02-02 15:19:17 -0600

    Done with adding cart items

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,5 +9,9 @@ import { Component } from '@angular/core';
 })
 export class CartComponent {
 
+  items = this.cartService.getItems();
 
+  constructor(
+    private cartService: CartService
+  ) { }
 }
