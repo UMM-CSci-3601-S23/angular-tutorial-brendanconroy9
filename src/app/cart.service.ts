bcefb950ac54b4882e180793f5930d44d56a1afd
Brendan Conroy <conro188@morris.umn.edu>
2023-02-02 15:25:30 -0600

    Done with retrieve shipping

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,5 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 /* . . . */
@@ -6,6 +8,10 @@ import { Injectable } from '@angular/core';
 })
 export class CartService {
   items: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
 /* . . . */
 
   addToCart(product: Product) {
@@ -21,4 +27,8 @@ export class CartService {
     return this.items;
   }
 /* . . . */
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
 }
