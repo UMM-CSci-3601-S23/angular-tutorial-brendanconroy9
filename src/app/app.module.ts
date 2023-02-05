a3c197714d97ad08118a1ee94b179af4b331defd
brendanconroy9 <112022206+brendanconroy9@users.noreply.github.com>
2023-02-04 23:11:49 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,8 +23,8 @@ import { ShippingComponent } from './shipping/shipping.component';
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
       { path: 'shipping', component: ShippingComponent },
-
     ])
+    ],{ useHash: true })
   ],
   declarations: [
     AppComponent,
